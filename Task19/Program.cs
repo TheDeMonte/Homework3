f5b968bdb486c55bb51e10372979b44921cd5cd0
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-23 00:38:06 +0500

    Small fixes to Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,18 +7,18 @@
 
 void Palindrome (int num)
 {
-    if (num >= 100000 || num < 10000) Console.WriteLine ("Введено неверное значение");
     int a = num / 10000;
     int b = (num / 1000) % 10;
     int B = (num % 100) / 10;
     int A = num % 10;
-    if (a == A && b == B)
+    if (num >= 100000 || num < 10000) Console.WriteLine ("Введено неверное значение");
+    else if (a == A && b == B)
     {
         Console.Write ($"Число {num} является палиндромом"); 
     }
     else
     {
-        Console.Write ("Число не является палиндромом");
+        Console.Write ($"Число {num} не является палиндромом");
     }
 }
 Console.WriteLine ("Введите пятизначное число: ");
